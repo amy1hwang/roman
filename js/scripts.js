be1dff8d950333fb3d2e8367a730f42863f37f44
Adam Baker and Amy Hwang <amy@>
2016-10-27 09:40:03 -0700

    add a return value for when the input is not a number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
 var romanFunction = function(num) {
-  var result = "";
-  var decimal = [1000, 500, 100, 50, 10, 5, 1];
-  var roman = ["M", "D", "C", "L", "X", "V", "I"];
-  for (var i = 0;i <= decimal.length;i++) {
-    while (num%decimal[i] < num) {
-      result += roman[i];
-      num -= decimal [i];
-    }
-  }
-  return result;
-}
+  if (isNaN(parseInt(num))) {
+    return "Enter a number"
+  } else {
+    var result = "";
+    var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV","I"];
+    for (var i = 0;i <= decimal.length;i++) {
+      while (num%decimal[i] < num) {
+        result += roman[i];
+        num -= decimal [i];
+      };
+    };
+    return result;
+  };
+};
 
 //4, 9 IV IX
 //1-3 is a group of I's
@@ -18,7 +22,7 @@ var romanFunction = function(num) {
 $(document).ready(function() {
   $("form#roman").submit(function(event){
     event.preventDefault();
-    var input = $("#input").val();
-    console.log(romanFunction(input));
+    var num = ($("#input").val());
+    $(".output").text(romanFunction(num));
   });
 });
